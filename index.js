c10a7384c3712d7c62d53e84c4dbc48baeb94a77
RandyQin <randyqin@haizol.com>
2021-01-05 15:26:08 +0800

    js测试

--- index.js
+++ index.js
@@ -21,3 +21,29 @@ Object.defineProperty(globalThis, 'b', {
 if(b===1&&b===2&&b===3) {
   console.log('success!!!');
 }
+
+
+
+console.log(({} + {}).length); // 30
+console.log({}.toString());
+/*
+  {}.toString =>  [object Object]
+  [object Object] + [object Object] => [object Object][object Object]
+*/
+
+
+console.log(([] + []).length); // 0
+console.log([1,2,3].toString()); // 1,2,3
+/*
+  [].toString = ''
+ */
+
+console.log((function(...args){}).length); // 0
+
+
+function test1(...args){}
+
+
+test1(1,2,3)
+
+console.log(test1.length); // 0
