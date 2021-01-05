9a9f8ceb06d43e31f427f85ee340258afee2abe7
qinran0423 <1156107187@qq.com>
2021-01-05 20:28:43 +0800

    this

---
+++ this/js/this1.js
@@ -0,0 +1,39 @@
+// 全局作用域下的this -> 全局对象
+// window 和 this 的关系
+
+/**
+ * 获取全局对象
+ * web: window, self, frames, this
+ * node: global
+ * worker: self,
+ * 通用： globalThis
+ */
+// console.log(this === window);
+
+
+// var a = 'global -> a'
+// var obj = {
+//   a: 'obj -> a',
+//   test: function() {
+//     console.log(this.a);
+//     console.log(globalThis.a);
+//     console.log(window.a);
+//     console.log(self.a);
+//     console.log(frames.a);
+//   }
+// }
+
+
+// obj.test()
+
+
+
+function test() {
+  'use strict';
+  return this
+}
+
+
+
+console.log(test());
+console.log(window.test());
---
+++ this/js/this2.js
@@ -0,0 +1,18 @@
+/**
+ * 类的本质就是函数
+ */
+
+  class Test {
+    constructor() {
+
+    }
+    say() {
+
+    }
+    static do() {
+
+    }
+  }
+
+
+  const test1 = new Test1()
