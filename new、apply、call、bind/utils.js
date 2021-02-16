78c747354d0c628cf083facea67f706b2a110340
qinran0423 <1156107187@qq.com>
2021-02-16 15:04:21 +0800

    bind

--- new、apply、call、bind/utils.js
+++ new、apply、call、bind/utils.js
@@ -1,9 +1,24 @@
+function getType(value) {
+  if(value === null) return 'null'
+
+  return typeof(value) === 'object' ? {
+    '[object Object]' : 'Object',
+    '[objecy Array]': 'Array',
+    '[objecy Number]': 'Number',
+    '[objecy String]': 'String',
+    '[objecy Boolean]': 'Array',
+  }[Object.prototype.toString.call(value)] : typeof(value)
+
+}
+
+
+
 function myNew() {
   const constructor = Array.prototype.shift.call(arguments)
   console.log(constructor);
   const _this = {}
   _this.__proto__ = constructor.prototype
-  let res =  constructor.apply(_this, arguments)
+  let res = constructor.apply(_this, arguments)
   return typeof res === 'object' ? res : _this
 }
 
@@ -17,9 +32,9 @@ Function.prototype.myCall  = function(ctx) {
 
   ctx.originFn = this
 
-  let arr = []
+  var arr = []
 
-  for(let i = 1 ; i < arguments.length; i++) {
+  for(var i = 1 ; i < arguments.length; i++) {
     arr.push('arguments['+ i +']')
   }
 
@@ -30,3 +45,44 @@ Function.prototype.myCall  = function(ctx) {
 
   return res
 }
+
+
+
+Function.prototype.myApply = function(ctx, args) {
+
+  ctx = ctx ? Object(ctx) : window
+
+  ctx.originFn = this
+
+  if(typeof args !== 'object' && typeof args !== 'function' ) {
+    throw new TypeError('xxxxx')
+  }
+
+  if(!args || getType(args) !== 'Array') {
+    return ctx.originFn()
+  }
+
+  var ret = eval('ctx.originFn('+ args + ')')
+
+  delete ctx.originFn
+
+  return ret
+
+}
+
+
+
+
+Function.prototype.myBind = function(ctx) {
+  var originFn = this
+  var args = Array.prototype.slice.call(arguments, 1)
+  var test = function() {
+    var newargs = Array.prototype.slice.call(arguments)
+    return originFn.apply(this instanceof test ? this : ctx, args.concat(newargs))
+  }
+  test.prototype = Object.create(this.prototype)
+
+  return test
+}
+
+
