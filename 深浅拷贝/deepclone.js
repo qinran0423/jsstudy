b10419429db877b1e6a815d2ff97d476a7258d7c
RandyQin <randyqin@haizol.com>
2021-02-20 14:30:43 +0800

    深拷贝

--- 深浅拷贝/deepclone.js
+++ 深浅拷贝/deepclone.js
@@ -7,19 +7,41 @@
  */
 
 
-const deepClone = (obj) => {
+const deepClone = (origin, hasmap = new WeakMap()) => {
+  if(origin == undefined || typeof origin !== 'object') {
+    return origin
+  }
   // date
-  if(obj.constructor === Date) {
-    return new Date(obj)
+  if(origin.constructor === Date) {
+    return new Date(origin)
   }
   // RegExp
-  if(obj.constructor === RegExp) {
-    return new RegExp(obj)
+  if(origin.constructor === RegExp) {
+    return new RegExp(origin)
   }
 
+  const hashKey = hasmap.get(origin)
+  if(hashKey) {
+    return hashKey
+  }
+  const cloneContext = new origin.constructor()
 
-  // 繼承原型鏈
-  
+  hasmap.set(origin, cloneContext)
+  for (const key in origin) {
+    if(origin.hasOwnProperty(key)) {
+      cloneContext[key] = deepClone(origin[key], hasmap)
+    }
+  }
+  console.log(hasmap);
+  return cloneContext
 }
 
+
+let test1 = {}
+let test2 = {}
+
+test2.test1 = test1
+test1.test2 = test2
+
+console.log(deepClone(test2));
 
