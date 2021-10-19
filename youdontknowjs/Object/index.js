ed7d312e4876f23a7c74f1cde33a68f329e8fdd4
qinran0423 <1156107187@qq.com>
2021-10-20 07:20:01 +0800

    Object study

--- youdontknowjs/Object/index.js
+++ youdontknowjs/Object/index.js
@@ -31,3 +31,70 @@ console.log(myArray.baz);
 
 
 
+var myObject = {
+  get a() {
+    return 2
+  }
+}
+
+
+Object.defineProperty(myObject, 'b', {
+  get: function () {
+    return this.a * 2
+  },
+  enumerable: true
+})
+
+
+console.log(myObject.a);
+
+console.log(myObject.b);
+
+
+// 存在性
+// 在不访问属性值的情况下判断对象中是否存在这个属性
+var myObject = {
+  a: 2
+}
+
+
+console.log('a' in myObject); // true
+console.log('b' in myObject); // false
+
+
+console.log(myObject.hasOwnProperty('a')); // true
+console.log(myObject.hasOwnProperty('b')); // false
+
+
+// in 检查属性是否在对象及其prototype原型链中
+// hasOwnProperty 检查属性是否在myObject对象中， 不会检查prototype
+
+var myObject = {
+  a: 2,
+  b: 3
+}
+
+Object.defineProperty(myObject, Symbol.iterator, {
+  enumerable: false,
+  writable: false,
+  configurable: true,
+  value: function () {
+    var o = this
+    console.log('this', this);
+    var idx = 0
+    var ks = Object.keys(o)
+    return {
+      next: function () {
+        return {
+          value: o[ks[idx++]],
+          done: (idx > ks.length)
+        }
+      }
+    }
+  }
+})
+
+var it = myObject[Symbol.iterator]()
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
